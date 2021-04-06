403801e9c799f1e09c16f984a515736ac66ecf7b
kzyhsgw <kzyhsgw@gmail.com>
2021-04-06 18:59:27 +0900

    itemモデルとtimesテーブルを作成

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :day_id
+    validates :price
+  end
+
+  belings_to :user
+end
