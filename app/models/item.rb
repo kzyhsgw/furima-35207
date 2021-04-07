44760eda3d93d8a64f9efcf3732df45419f78772
kzyhsgw <kzyhsgw@gmail.com>
2021-04-07 12:09:08 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,21 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
-    validates :category_id
-    validates :condition_id
-    validates :postage_id
-    validates :prefecture_id
-    validates :day_id
+    validates :category_id numericality: { other_than: 1 }
+    validates :condition_id numericality: { other_than: 1 }
+    validates :postage_id numericality: { other_than: 1 }
+    validates :prefecture_id numericality: { other_than: 1 }
+    validates :day_id numericality: { other_than: 1 }
     validates :price
   end
 
   belings_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :day
 end
