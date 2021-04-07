b70fc452bd17524c864fa02c4fedf097a8b1b637
kzyhsgw <kzyhsgw@gmail.com>
2021-04-07 12:28:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,24 @@
 class Item < ApplicationRecord
-  with_options presence: true do
-    validates :name
-    validates :text
-    validates :category_id numericality: { other_than: 1 }
-    validates :condition_id numericality: { other_than: 1 }
-    validates :postage_id numericality: { other_than: 1 }
-    validates :prefecture_id numericality: { other_than: 1 }
-    validates :day_id numericality: { other_than: 1 }
-    validates :price
-  end
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belings_to :user
   has_one_attached :image
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :postage
   belongs_to :prefecture
   belongs_to :day
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :postage_id
+      validates :prefecture_id
+      validates :day_id
+    end
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+  end
 end
