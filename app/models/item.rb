3f641c07a7c69105aac369fbffe336747be31881
kzyhsgw <kzyhsgw@gmail.com>
2021-04-26 18:19:52 +0900

    基本投稿機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :order
+  has_many :comments
   has_many_attached :images
   belongs_to :category
   belongs_to :condition
